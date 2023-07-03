eade7fa19c55cc0dbe302086dad4f1734f439777
goloop <goloop.one@gmail.com>
2023-07-03 22:18:21 +0300

    Pre-production version

--- layout/layout_test.go
+++ layout/layout_test.go
@@ -5,16 +5,33 @@ import (
 	"testing"
 )
 
+// TestIsValid tests the IsValid method of the Layout type.
 func TestIsValid(t *testing.T) {
 	tests := []struct {
 		name   string
 		Layout Layout
 		expect bool
 	}{
-		{name: "FullFilePath Layout", Layout: FullFilePath, expect: true},
-		{name: "ShortFilePath Layout", Layout: ShortFilePath, expect: true},
-		{name: "FuncName Layout", Layout: FuncName, expect: true},
-		{name: "Overflow Layout", Layout: overflowLayoutValue + 1, expect: false},
+		{
+			name:   "FullFilePath Layout",
+			Layout: FullFilePath,
+			expect: true,
+		},
+		{
+			name:   "ShortFilePath Layout",
+			Layout: ShortFilePath,
+			expect: true,
+		},
+		{
+			name:   "FuncName Layout",
+			Layout: FuncName,
+			expect: true,
+		},
+		{
+			name:   "Overflow Layout",
+			Layout: overflowLayoutValue + 1,
+			expect: false,
+		},
 	}
 
 	for _, test := range tests {
@@ -26,15 +43,23 @@ func TestIsValid(t *testing.T) {
 	}
 }
 
+// TestIsSingle tests the IsSingle method of the Layout type.
 func TestIsSingle(t *testing.T) {
 	tests := []struct {
 		name   string
 		Layout Layout
 		expect bool
 	}{
-		{name: "FullFilePath Layout", Layout: FullFilePath, expect: true},
-		{name: "Combined Layout", Layout: FullFilePath | ShortFilePath, expect: false},
-		// add more tests as needed
+		{
+			name:   "FullFilePath Layout",
+			Layout: FullFilePath,
+			expect: true,
+		},
+		{
+			name:   "Combined Layout",
+			Layout: FullFilePath | ShortFilePath,
+			expect: false,
+		},
 	}
 
 	for _, test := range tests {
@@ -46,6 +71,7 @@ func TestIsSingle(t *testing.T) {
 	}
 }
 
+// TestContains tests the Contains method of the Layout type.
 func TestContains(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -54,21 +80,34 @@ func TestContains(t *testing.T) {
 		expect bool
 		err    error
 	}{
-		{name: "FullFilePath Layout contains FullFilePath", Layout: FullFilePath, flag: FullFilePath, expect: true, err: nil},
-		{name: "FullFilePath Layout contains FuncName", Layout: FullFilePath, flag: FuncName, expect: false, err: nil},
-		// add more tests as needed
+		{
+			name:   "FullFilePath Layout contains FullFilePath",
+			Layout: FullFilePath,
+			flag:   FullFilePath,
+			expect: true,
+			err:    nil,
+		},
+		{
+			name:   "FullFilePath Layout contains FuncName",
+			Layout: FullFilePath,
+			flag:   FuncName,
+			expect: false,
+			err:    nil,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := test.Layout.Contains(test.flag)
 			if err != test.err || got != test.expect {
-				t.Errorf("Contains = %v, FuncName = %v; want %v, FuncName = %v", got, err, test.expect, test.err)
+				t.Errorf("Contains = %v, FuncName = %v; "+
+					"want %v, FuncName = %v", got, err, test.expect, test.err)
 			}
 		})
 	}
 }
 
+// TestLayoutFlags tests the methods that check for flags in the Layout type.
 func TestLayoutFlags(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -76,13 +115,48 @@ func TestLayoutFlags(t *testing.T) {
 		method  func(*Layout) bool
 		contain bool
 	}{
-		{name: "FullFilePath Flag in FullFilePath Layout", Layout: FullFilePath, method: (*Layout).FullFilePath, contain: true},
-		{name: "FullFilePath Flag in FuncName Layout", Layout: FuncName, method: (*Layout).FullFilePath, contain: false},
-		{name: "ShortFilePath Flag in ShortFilePath Layout", Layout: ShortFilePath, method: (*Layout).ShortFilePath, contain: true},
-		{name: "ShortFilePath Flag in FuncName Layout", Layout: FuncName, method: (*Layout).ShortFilePath, contain: false},
-		{name: "FuncName Flag in FuncName Layout", Layout: FuncName, method: (*Layout).FuncName, contain: true},
-		{name: "FuncName Flag in FuncAddress Layout", Layout: FuncAddress, method: (*Layout).FuncName, contain: false},
-		{name: "FuncAddress Flag in FuncAddress Layout", Layout: FuncAddress, method: (*Layout).FuncAddress, contain: true},
+		{
+			name:    "FullFilePath Flag in FullFilePath Layout",
+			Layout:  FullFilePath,
+			method:  (*Layout).FullFilePath,
+			contain: true,
+		},
+		{
+			name:    "FullFilePath Flag in FuncName Layout",
+			Layout:  FuncName,
+			method:  (*Layout).FullFilePath,
+			contain: false,
+		},
+		{
+			name:    "ShortFilePath Flag in ShortFilePath Layout",
+			Layout:  ShortFilePath,
+			method:  (*Layout).ShortFilePath,
+			contain: true,
+		},
+		{
+			name:    "ShortFilePath Flag in FuncName Layout",
+			Layout:  FuncName,
+			method:  (*Layout).ShortFilePath,
+			contain: false,
+		},
+		{
+			name:    "FuncName Flag in FuncName Layout",
+			Layout:  FuncName,
+			method:  (*Layout).FuncName,
+			contain: true,
+		},
+		{
+			name:    "FuncName Flag in FuncAddress Layout",
+			Layout:  FuncAddress,
+			method:  (*Layout).FuncName,
+			contain: false,
+		},
+		{
+			name:    "FuncAddress Flag in FuncAddress Layout",
+			Layout:  FuncAddress,
+			method:  (*Layout).FuncAddress,
+			contain: true,
+		},
 	}
 
 	for _, test := range tests {
@@ -94,6 +168,7 @@ func TestLayoutFlags(t *testing.T) {
 	}
 }
 
+// TestSet tests the Set method of the Layout type.
 func TestSet(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -102,20 +177,27 @@ func TestSet(t *testing.T) {
 		expect Layout
 		err    error
 	}{
-		{name: "Set FullFilePath and FuncName flags", Layout: Default, flags: []Layout{FullFilePath, FuncName}, expect: FullFilePath | FuncName, err: nil},
-		// add more tests as needed
+		{
+			name:   "Set FullFilePath and FuncName flags",
+			Layout: Default,
+			flags:  []Layout{FullFilePath, FuncName},
+			expect: FullFilePath | FuncName,
+			err:    nil,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := test.Layout.Set(test.flags...)
 			if err != test.err || got != test.expect {
-				t.Errorf("Set = %v, FuncName = %v; want %v, FuncName = %v", got, err, test.expect, test.err)
+				t.Errorf("Set = %v, FuncName = %v; want %v, FuncName = %v",
+					got, err, test.expect, test.err)
 			}
 		})
 	}
 }
 
+// TestLayoutAdd tests the Add method of the Layout type.
 func TestLayoutAdd(t *testing.T) {
 	tests := []struct {
 		name  string
@@ -124,22 +206,41 @@ func TestLayoutAdd(t *testing.T) {
 		want  Layout
 		err   error
 	}{
-		{name: "Add FullFilePath to Empty Layout", start: 0, add: []Layout{FullFilePath}, want: FullFilePath, err: nil},
-		{name: "Add FullFilePath to FullFilePath Layout", start: FullFilePath, add: []Layout{FullFilePath}, want: FullFilePath, err: nil},
-		{name: "Add FuncName to FullFilePath Layout", start: FullFilePath, add: []Layout{FuncName}, want: FullFilePath | FuncName, err: nil},
-		//{name: "Add Invalid Flag", start: FullFilePath, add: []Layout{overflowLayoutValue}, want: FullFilePath, err: fmt.Errorf("the %d is invalid flag value", overflowLayoutValue)},
+		{
+			name:  "Add FullFilePath to Empty Layout",
+			start: 0,
+			add:   []Layout{FullFilePath},
+			want:  FullFilePath,
+			err:   nil,
+		},
+		{
+			name:  "Add FullFilePath to FullFilePath Layout",
+			start: FullFilePath,
+			add:   []Layout{FullFilePath},
+			want:  FullFilePath,
+			err:   nil,
+		},
+		{
+			name:  "Add FuncName to FullFilePath Layout",
+			start: FullFilePath,
+			add:   []Layout{FuncName},
+			want:  FullFilePath | FuncName,
+			err:   nil,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := test.start.Add(test.add...)
 			if got != test.want || !errors.Is(err, test.err) {
-				t.Errorf("%s got = %v, err = %v; want %v, %v", test.name, got, err, test.want, test.err)
+				t.Errorf("%s got = %v, err = %v; want %v, %v",
+					test.name, got, err, test.want, test.err)
 			}
 		})
 	}
 }
 
+// TestLayoutDelete tests the Delete method of the Layout type.
 func TestLayoutDelete(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -148,21 +249,34 @@ func TestLayoutDelete(t *testing.T) {
 		want   Layout
 		err    error
 	}{
-		{name: "Delete FullFilePath from FullFilePath Layout", start: FullFilePath, delete: []Layout{FullFilePath}, want: 0, err: nil},
-		{name: "Delete FullFilePath from FuncName Layout", start: FuncName, delete: []Layout{FullFilePath}, want: FuncName, err: nil},
-		//{name: "Delete Invalid Flag", start: FullFilePath, delete: []Layout{overflowLayoutValue}, want: FullFilePath, err: fmt.Errorf("the %d is invalid flag value", overflowLayoutValue)},
+		{
+			name:   "Delete FullFilePath from FullFilePath Layout",
+			start:  FullFilePath,
+			delete: []Layout{FullFilePath},
+			want:   0,
+			err:    nil,
+		},
+		{
+			name:   "Delete FullFilePath from FuncName Layout",
+			start:  FuncName,
+			delete: []Layout{FullFilePath},
+			want:   FuncName,
+			err:    nil,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := test.start.Delete(test.delete...)
 			if got != test.want || !errors.Is(err, test.err) {
-				t.Errorf("%s got = %v, err = %v; want %v, %v", test.name, got, err, test.want, test.err)
+				t.Errorf("%s got = %v, err = %v; want %v, %v",
+					test.name, got, err, test.want, test.err)
 			}
 		})
 	}
 }
 
+// TestLayoutAll tests the All method of the Layout type.
 func TestLayoutAll(t *testing.T) {
 	tests := []struct {
 		name  string
@@ -170,8 +284,19 @@ func TestLayoutAll(t *testing.T) {
 		all   []Layout
 		want  bool
 	}{
-		{name: "All FullFilePath and FuncName in FullFilePath Layout", start: FullFilePath, all: []Layout{FullFilePath, FuncName}, want: false},
-		{name: "All FullFilePath and FuncName in FullFilePath and FuncName Layout", start: FullFilePath | FuncName, all: []Layout{FullFilePath, FuncName}, want: true},
+		{
+			name:  "All FullFilePath and FuncName in FullFilePath Layout",
+			start: FullFilePath,
+			all:   []Layout{FullFilePath, FuncName},
+			want:  false,
+		},
+		{
+			name: "All FullFilePath and FuncName in " +
+				"FullFilePath and FuncName Layout",
+			start: FullFilePath | FuncName,
+			all:   []Layout{FullFilePath, FuncName},
+			want:  true,
+		},
 	}
 
 	for _, test := range tests {
@@ -183,6 +308,7 @@ func TestLayoutAll(t *testing.T) {
 	}
 }
 
+// TestLayoutAny tests the Any method of the Layout type.
 func TestLayoutAny(t *testing.T) {
 	tests := []struct {
 		name  string
@@ -190,8 +316,18 @@ func TestLayoutAny(t *testing.T) {
 		any   []Layout
 		want  bool
 	}{
-		{name: "Any FullFilePath and FuncName in FullFilePath Layout", start: FullFilePath, any: []Layout{FullFilePath, FuncName}, want: true},
-		{name: "Any FullFilePath and FuncName in FuncAddress Layout", start: FuncAddress, any: []Layout{FullFilePath, FuncName}, want: false},
+		{
+			name:  "Any FullFilePath and FuncName in FullFilePath Layout",
+			start: FullFilePath,
+			any:   []Layout{FullFilePath, FuncName},
+			want:  true,
+		},
+		{
+			name:  "Any FullFilePath and FuncName in FuncAddress Layout",
+			start: FuncAddress,
+			any:   []Layout{FullFilePath, FuncName},
+			want:  false,
+		},
 	}
 
 	for _, test := range tests {
--- level/level_test.go
+++ level/level_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// TestIsValid tests the IsValid method of the Level type.
 func TestIsValid(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -26,6 +27,7 @@ func TestIsValid(t *testing.T) {
 	}
 }
 
+// TestIsSingle tests the IsSingle method of the Level type.
 func TestIsSingle(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -46,6 +48,7 @@ func TestIsSingle(t *testing.T) {
 	}
 }
 
+// TestContains tests the Contains method of the Level type.
 func TestContains(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -54,21 +57,34 @@ func TestContains(t *testing.T) {
 		expect bool
 		err    error
 	}{
-		{name: "Panic Level contains Panic", level: Panic, flag: Panic, expect: true, err: nil},
-		{name: "Panic Level contains Error", level: Panic, flag: Error, expect: false, err: nil},
-		// add more tests as needed
+		{
+			name:   "Panic Level contains Panic",
+			level:  Panic,
+			flag:   Panic,
+			expect: true,
+			err:    nil,
+		},
+		{
+			name:   "Panic Level contains Error",
+			level:  Panic,
+			flag:   Error,
+			expect: false,
+			err:    nil,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := test.level.Contains(test.flag)
 			if err != test.err || got != test.expect {
-				t.Errorf("Contains = %v, error = %v; want %v, error = %v", got, err, test.expect, test.err)
+				t.Errorf("Contains = %v, error = %v; want %v, error = %v",
+					got, err, test.expect, test.err)
 			}
 		})
 	}
 }
 
+// TestLevelFlags tests the level flags.
 func TestLevelFlags(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -76,19 +92,78 @@ func TestLevelFlags(t *testing.T) {
 		method  func(*Level) bool
 		contain bool
 	}{
-		{name: "Panic Flag in Panic Level", level: Panic, method: (*Level).Panic, contain: true},
-		{name: "Panic Flag in Error Level", level: Error, method: (*Level).Panic, contain: false},
-		{name: "Fatal Flag in Fatal Level", level: Fatal, method: (*Level).Fatal, contain: true},
-		{name: "Fatal Flag in Error Level", level: Error, method: (*Level).Fatal, contain: false},
-		{name: "Error Flag in Error Level", level: Error, method: (*Level).Error, contain: true},
-		{name: "Error Flag in Info Level", level: Info, method: (*Level).Error, contain: false},
-		{name: "Info Flag in Info Level", level: Info, method: (*Level).Info, contain: true},
-		{name: "Info Flag in Debug Level", level: Debug, method: (*Level).Info, contain: false},
-		{name: "Debug Flag in Debug Level", level: Debug, method: (*Level).Debug, contain: true},
-		{name: "Debug Flag in Trace Level", level: Trace, method: (*Level).Debug, contain: false},
-		{name: "Trace Flag in Trace Level", level: Trace, method: (*Level).Trace, contain: true},
-		{name: "Trace Flag in Panic Level", level: Panic, method: (*Level).Trace, contain: false},
-		// add more tests as needed
+		{
+			name:    "Panic Flag in Panic Level",
+			level:   Panic,
+			method:  (*Level).Panic,
+			contain: true,
+		},
+		{
+			name:    "Panic Flag in Error Level",
+			level:   Error,
+			method:  (*Level).Panic,
+			contain: false,
+		},
+		{
+			name:    "Fatal Flag in Fatal Level",
+			level:   Fatal,
+			method:  (*Level).Fatal,
+			contain: true,
+		},
+		{
+			name:    "Fatal Flag in Error Level",
+			level:   Error,
+			method:  (*Level).Fatal,
+			contain: false,
+		},
+		{
+			name:    "Error Flag in Error Level",
+			level:   Error,
+			method:  (*Level).Error,
+			contain: true,
+		},
+		{
+			name:    "Error Flag in Info Level",
+			level:   Info,
+			method:  (*Level).Error,
+			contain: false,
+		},
+		{
+			name:    "Info Flag in Info Level",
+			level:   Info,
+			method:  (*Level).Info,
+			contain: true,
+		},
+		{
+			name:    "Info Flag in Debug Level",
+			level:   Debug,
+			method:  (*Level).Info,
+			contain: false,
+		},
+		{
+			name:    "Debug Flag in Debug Level",
+			level:   Debug,
+			method:  (*Level).Debug,
+			contain: true,
+		},
+		{
+			name:    "Debug Flag in Trace Level",
+			level:   Trace,
+			method:  (*Level).Debug,
+			contain: false,
+		},
+		{
+			name:    "Trace Flag in Trace Level",
+			level:   Trace,
+			method:  (*Level).Trace,
+			contain: true,
+		},
+		{
+			name:    "Trace Flag in Panic Level",
+			level:   Panic,
+			method:  (*Level).Trace,
+			contain: false,
+		},
 	}
 
 	for _, test := range tests {
@@ -100,6 +175,7 @@ func TestLevelFlags(t *testing.T) {
 	}
 }
 
+// TestSet tests the Set method of Level.
 func TestSet(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -108,15 +184,21 @@ func TestSet(t *testing.T) {
 		expect Level
 		err    error
 	}{
-		{name: "Set Panic and Error flags", level: Default, flags: []Level{Panic, Error}, expect: Panic | Error, err: nil},
-		// add more tests as needed
+		{
+			name:   "Set Panic and Error flags",
+			level:  Default,
+			flags:  []Level{Panic, Error},
+			expect: Panic | Error,
+			err:    nil,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := test.level.Set(test.flags...)
 			if err != test.err || got != test.expect {
-				t.Errorf("Set = %v, error = %v; want %v, error = %v", got, err, test.expect, test.err)
+				t.Errorf("Set = %v, error = %v; want %v, error = %v",
+					got, err, test.expect, test.err)
 			}
 		})
 	}
@@ -130,22 +212,41 @@ func TestLevelAdd(t *testing.T) {
 		want  Level
 		err   error
 	}{
-		{name: "Add Panic to Empty Level", start: 0, add: []Level{Panic}, want: Panic, err: nil},
-		{name: "Add Panic to Panic Level", start: Panic, add: []Level{Panic}, want: Panic, err: nil},
-		{name: "Add Error to Panic Level", start: Panic, add: []Level{Error}, want: Panic | Error, err: nil},
-		//{name: "Add Invalid Flag", start: Panic, add: []Level{overflowLevelValue}, want: Panic, err: fmt.Errorf("the %d is invalid flag value", overflowLevelValue)},
+		{
+			name:  "Add Panic to Empty Level",
+			start: 0,
+			add:   []Level{Panic},
+			want:  Panic,
+			err:   nil,
+		},
+		{
+			name:  "Add Panic to Panic Level",
+			start: Panic,
+			add:   []Level{Panic},
+			want:  Panic,
+			err:   nil,
+		},
+		{
+			name:  "Add Error to Panic Level",
+			start: Panic,
+			add:   []Level{Error},
+			want:  Panic | Error,
+			err:   nil,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := test.start.Add(test.add...)
 			if got != test.want || !errors.Is(err, test.err) {
-				t.Errorf("%s got = %v, err = %v; want %v, %v", test.name, got, err, test.want, test.err)
+				t.Errorf("%s got = %v, err = %v; want %v, %v",
+					test.name, got, err, test.want, test.err)
 			}
 		})
 	}
 }
 
+// TestLevelDelete tests the Delete method of Level.
 func TestLevelDelete(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -154,21 +255,34 @@ func TestLevelDelete(t *testing.T) {
 		want   Level
 		err    error
 	}{
-		{name: "Delete Panic from Panic Level", start: Panic, delete: []Level{Panic}, want: 0, err: nil},
-		{name: "Delete Panic from Error Level", start: Error, delete: []Level{Panic}, want: Error, err: nil},
-		//{name: "Delete Invalid Flag", start: Panic, delete: []Level{overflowLevelValue}, want: Panic, err: fmt.Errorf("the %d is invalid flag value", overflowLevelValue)},
+		{
+			name:   "Delete Panic from Panic Level",
+			start:  Panic,
+			delete: []Level{Panic},
+			want:   0,
+			err:    nil,
+		},
+		{
+			name:   "Delete Panic from Error Level",
+			start:  Error,
+			delete: []Level{Panic},
+			want:   Error,
+			err:    nil,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := test.start.Delete(test.delete...)
 			if got != test.want || !errors.Is(err, test.err) {
-				t.Errorf("%s got = %v, err = %v; want %v, %v", test.name, got, err, test.want, test.err)
+				t.Errorf("%s got = %v, err = %v; want %v, %v",
+					test.name, got, err, test.want, test.err)
 			}
 		})
 	}
 }
 
+// TestLevelAll tests the All method of Level.
 func TestLevelAll(t *testing.T) {
 	tests := []struct {
 		name  string
@@ -176,8 +290,18 @@ func TestLevelAll(t *testing.T) {
 		all   []Level
 		want  bool
 	}{
-		{name: "All Panic and Error in Panic Level", start: Panic, all: []Level{Panic, Error}, want: false},
-		{name: "All Panic and Error in Panic and Error Level", start: Panic | Error, all: []Level{Panic, Error}, want: true},
+		{
+			name:  "All Panic and Error in Panic Level",
+			start: Panic,
+			all:   []Level{Panic, Error},
+			want:  false,
+		},
+		{
+			name:  "All Panic and Error in Panic and Error Level",
+			start: Panic | Error,
+			all:   []Level{Panic, Error},
+			want:  true,
+		},
 	}
 
 	for _, test := range tests {
@@ -189,6 +313,7 @@ func TestLevelAll(t *testing.T) {
 	}
 }
 
+// TestLevelAny tests the Any method of Level.
 func TestLevelAny(t *testing.T) {
 	tests := []struct {
 		name  string
@@ -196,8 +321,18 @@ func TestLevelAny(t *testing.T) {
 		any   []Level
 		want  bool
 	}{
-		{name: "Any Panic and Error in Panic Level", start: Panic, any: []Level{Panic, Error}, want: true},
-		{name: "Any Panic and Error in Info Level", start: Info, any: []Level{Panic, Error}, want: false},
+		{
+			name:  "Any Panic and Error in Panic Level",
+			start: Panic,
+			any:   []Level{Panic, Error},
+			want:  true,
+		},
+		{
+			name:  "Any Panic and Error in Info Level",
+			start: Info,
+			any:   []Level{Panic, Error},
+			want:  false,
+		},
 	}
 
 	for _, test := range tests {
